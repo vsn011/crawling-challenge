53de63f4206f194b7ecad915dd30bd2d83e72b52
vsn011 <m.gogic.fb@gmail.com>
2022-04-14 15:39:32 +0200

    deleted commented rows

--- data_engineering/assignment/data_engine.py
+++ data_engineering/assignment/data_engine.py
@@ -14,17 +14,12 @@ args = parser.parse_args()
 logging.basicConfig(level=logging.DEBUG if args.verbose else logging.INFO)
 
 
-#df = read_jsonl(args.input)
+
 duplicate_finder = Duplicate_finder(args.input, config.key_columns)
 
-# logging.info('finding duplicate listings')
-# df_d = _get_df_with_duplicates(df, config.key_columns)
 
-
-# logging.info('creating list of duplicates')
-# final_list=df_to_list(df_d, config.key_columns)
 final_list= duplicate_finder.df_to_list()
-print(len(final_list))
+
 logging.info('storing duplicate list')
 with open(args.output, "w") as fp:
         json.dump(final_list, fp, indent=4)
